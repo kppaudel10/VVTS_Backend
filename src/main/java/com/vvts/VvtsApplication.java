dbaec5046a6cf2dfc8af62625bb0b7c9575bfc5b
Kul paudel <kppaudel56@gmail.com>
2023-04-07 13:02:04 +0545

    feat: initial project setup

---
+++ src/main/java/com/vvts/VvtsApplication.java
@@ -0,0 +1,13 @@
+package com.vvts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VvtsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VvtsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vvts/VvtsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vvts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VvtsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
